ba694bf1dcd442dd6bdc9fcb92c08f20f30b84e9
Sam Wei <samw2k00@gmail.com>
2022-08-23 12:17:01 +1000

    check nbf if exist

--- src/MockServices/DhServer/Server/Handlers/PushedAuthorizationRequest.ts
+++ src/MockServices/DhServer/Server/Handlers/PushedAuthorizationRequest.ts
@@ -2,7 +2,7 @@ import _ from "lodash";
 import express from "express";
 import { NextFunction } from "connect";
 
-import { validationResult, matchedData, check, body} from 'express-validator'
+import { validationResult, matchedData, check, body } from 'express-validator'
 import { inject, injectable } from "tsyringe";
 import { JWT, JWKS } from "jose";
 import bodyParser from "body-parser";
@@ -13,9 +13,9 @@ import { Dictionary } from "../../../../Common/Server/Types";
 import uuid from "uuid";
 import moment from 'moment'
 
-const storedRequests:Dictionary<string> = {};
+const storedRequests: Dictionary<string> = {};
 
-export const GetStagedRequestById = (id:string) => {
+export const GetStagedRequestById = (id: string) => {
     return storedRequests[id]
 }
 
@@ -24,24 +24,24 @@ export class PushedAuthorizationRequestMiddleware {
     constructor(
         private clientRegistrationManager: ClientRegistrationManager,
         @inject("ClientCertificateInjector") private mtls: ClientCertificateInjector,
-    ){}
+    ) { }
 
 
     handler = () => {
-        let validationErrorMiddleware = (req:express.Request,res:express.Response,next: NextFunction) => {
+        let validationErrorMiddleware = (req: express.Request, res: express.Response, next: NextFunction) => {
             const errors = validationResult(req);
             if (!errors.isEmpty()) {
-              return res.status(400).json({ error: "invalid_request" });
+                return res.status(400).json({ error: "invalid_request" });
             }
             next();
         }
-    
-        let Responder = async (req:express.Request,res:express.Response,next: NextFunction) => {
-    
-            res.setHeader('Cache-Control','no-store')
-            res.setHeader('Pragma','no-cache')
-            
-            let params:{
+
+        let Responder = async (req: express.Request, res: express.Response, next: NextFunction) => {
+
+            res.setHeader('Cache-Control', 'no-store')
+            res.setHeader('Pragma', 'no-cache')
+
+            let params: {
                 request: string
                 client_id: string,
                 client_assertion: string,
@@ -52,75 +52,76 @@ export class PushedAuthorizationRequestMiddleware {
             // TODO move this client credntial check to an auth middleware
             let client = await this.clientRegistrationManager.GetRegistration(params.client_id);
 
-            if (typeof client == 'undefined') return res.status(401).json({error:"invalid_client"});
+            if (typeof client == 'undefined') return res.status(401).json({ error: "invalid_client" });
 
             // GET the JWKS for signing
-            let client_jwks = JWKS.asKeyStore(await (await axios.get(client.jwks_uri, this.mtls.injectCa({responseType:"json"}))).data)
+            let client_jwks = JWKS.asKeyStore(await (await axios.get(client.jwks_uri, this.mtls.injectCa({ responseType: "json" }))).data)
 
             // verify the JWT
-            let payload:any;
+            let payload: any;
             try {
-                payload = JWT.verify(params.client_assertion,client_jwks,{algorithms:["PS256"]})
-                for (let key of ['aud','jti','exp','iss','sub']){
-                    if (typeof payload[key] === 'undefined')  {
+                payload = JWT.verify(params.client_assertion, client_jwks, { algorithms: ["PS256"] })
+                for (let key of ['aud', 'jti', 'exp', 'iss', 'sub']) {
+                    if (typeof payload[key] === 'undefined') {
                         throw `key ${key} is missing from JWT`
                     }
-                } 
+                }
 
                 if (payload.sub !== params.client_id) throw "client_id mismatch"
             } catch (e) {
-                return res.status(401).json({error:"invalid_client"})
+                return res.status(401).json({ error: "invalid_client" })
             }
 
             // verify the requestObject JWT
-            let requestObject:any;
+            let requestObject: any;
             try {
                 // payload = JWT.decode(params.request)
-                requestObject = JWT.verify(params.request,client_jwks,{algorithms:["PS256"]})
-                for (let key of ['aud','exp','iss','nbf']) {// add nbf BUG 2919
-                    if (typeof requestObject[key] === 'undefined')  {
+                requestObject = JWT.verify(params.request, client_jwks, { algorithms: ["PS256"] })
+                for (let key of ['aud', 'exp', 'iss', 'nbf']) {// add nbf BUG 2919
+                    if (typeof requestObject[key] === 'undefined') {
                         throw `key ${key} is missing from JWT`
-                    }    
+                    }
                 }
-
-                // BUG 2919 NBF (not before flag) // TODO
-                if(!moment(requestObject['nbf']).isSameOrBefore(moment())){
-                    throw `nbf is after current time` 
+                if (requestObject['nbf']) {
+                    // BUG 2919 NBF (not before flag)
+                    if (!moment(requestObject['nbf']).isSameOrBefore(moment())) {
+                        throw `nbf is after current time`
+                    }
                 }
                 if (requestObject.client_id !== params.client_id) throw "client_id mismatch"
             } catch (e) {
-                return res.status(401).json({error:"invalid_client"})
+                return res.status(401).json({ error: "invalid_client" })
             }
 
             if (typeof params.scope !== "string" || params.scope !== requestObject.scope) {
-                return res.status(401).json({error:"invalid_request"});
+                return res.status(401).json({ error: "invalid_request" });
             }
 
             if (typeof params.response_type !== "string" || params.response_type !== requestObject.response_type) {
-                return res.status(401).json({error:"invalid_request"});
+                return res.status(401).json({ error: "invalid_request" });
             }
 
-            const request_uri = "par:"+uuid.v4();
+            const request_uri = "par:" + uuid.v4();
 
             storedRequests[request_uri] = requestObject;
             return res.json({
                 request_uri,
                 expiry: 90 // this is not enforced
             })
- 
+
         };
-    
+
         // decide whether to validate based on body or query parameters
         // TODO add client authorization
         return _.concat([
-            bodyParser.urlencoded({extended:true}),
+            bodyParser.urlencoded({ extended: true }),
             body("request").isJWT(),
             body("client_id").isString(),
             body('client_assertion_type').isString().equals("urn:ietf:params:oauth:client-assertion-type:jwt-bearer").withMessage("invalid client_assertion_type"),
             body("client_assertion").isJWT(),
             body("scope").optional().isString(),
             body("response_type").optional().isString()
-        ],[
+        ], [
             <any>validationErrorMiddleware,
             Responder
         ])
--- src/MockServices/Register/MockData/DataHolders.ts
+++ src/MockServices/Register/MockData/DataHolders.ts
@@ -65,7 +65,147 @@ export const DataHolders = async (config:MockRegisterConfig,pw:DefaultConnector)
     }
 
 
+    const realDhs = [
+        {
+          "dataHolderBrandId": "8c5bdd1b-aed9-40eb-8866-bfc5c1cfcae7",
+          "brandName": "Westpac",
+          "industry": "banking",
+          "logoUri": "https://banking.westpac.com.au/wbc/banking/Themes/Default/Desktop/WBC/Core/Images/logo_white_bg.png.6c772a263bf42d99a2c098bf0739fc5b504ed28d.png",
+          "legalEntity": {
+            "legalEntityId": "25017347-3adb-4fd6-8e73-dbec8d627cbe",
+            "legalEntityName": "Westpac Banking Corporation",
+            "logoUri": "https://banking.westpac.com.au/wbc/banking/Themes/Default/Desktop/WBC/Core/Images/logo_white_bg.png.ce5c4c19ec61b56796f0e218fc8329c558421fd8.png",
+            "abn": "33007457141",
+            "acn": "007457141"
+          },
+          "status": "ACTIVE",
+          "endpointDetail": {
+            "publicBaseUri": "https://digital-api.westpac.com.au/",
+            "resourceBaseUri": "https://cdr.api.westpac.com.au",
+            "infosecBaseUri": "https://cdr.idp.westpac.com.au/identity",
+            "websiteUri": "https://www.westpac.com.au/"
+          },
+          "authDetails": [
+            {
+              "registerUType": "SIGNED-JWT",
+              "jwksEndpoint": "https://idp.westpac.com.au/identity/ext/oauth/obdr_jwks"
+            }
+          ],
+          "lastUpdated": "2021-01-29T10:57:50Z"
+        },
+        {
+          "dataHolderBrandId": "89d926e2-78e8-4249-b92a-6c3d36839fe2",
+          "brandName": "NATIONAL AUSTRALIA BANK",
+          "industry": "banking",
+          "logoUri": "https://www.nab.com.au/etc/designs/nabrwd/clientlibs/images/logo.png",
+          "legalEntity": {
+            "legalEntityId": "9fc5dbfe-faf2-44ba-af92-9ab002b506f3",
+            "legalEntityName": "NATIONAL AUSTRALIA BANK LIMITED",
+            "logoUri": "https://www.nab.com.au/etc/designs/nabrwd/clientlibs/images/logo.png",
+            "abn": "12 004 044 937",
+            "acn": "004 044 9"
+          },
+          "status": "ACTIVE",
+          "endpointDetail": {
+            "publicBaseUri": "https://openbank.api.nab.com.au",
+            "resourceBaseUri": "https://openbank-secure.api.nab.com.au",
+            "infosecBaseUri": "https://openbank.api.nab.com.au",
+            "websiteUri": "https://developer.nab.com.au"
+          },
+          "authDetails": [
+            {
+              "registerUType": "SIGNED-JWT",
+              "jwksEndpoint": "https://openbank.api.nab.com.au/.well-known/keyset"
+            }
+          ],
+          "lastUpdated": "2021-03-02T06:05:16Z"
+        },
+        {
+          "dataHolderBrandId": "3150628b-cc24-40c6-be91-d599a1c35567",
+          "brandName": "ANZ",
+          "industry": "banking",
+          "logoUri": "https://www.anz.com.au/content/dam/anzcomau/logos/anz/ANZ-MB-Logo-3rd-Party-RGB.png",
+          "legalEntity": {
+            "legalEntityId": "73dadf05-5cf3-4135-8941-31f24a288df1",
+            "legalEntityName": "Australia and New Zealand Banking Group Limited",
+            "logoUri": "https://www.anz.com.au/content/dam/anzcomau/logos/anz/ANZ-MB-Logo-3rd-Party-RGB.png",
+            "abn": "11005357522",
+            "acn": "005357522"
+          },
+          "status": "ACTIVE",
+          "endpointDetail": {
+            "publicBaseUri": "https://api.anz",
+            "resourceBaseUri": "https://cdr.api.anz",
+            "infosecBaseUri": "https://unauth.cdr.api.anz",
+            "websiteUri": "https://www.anz.com.au"
+          },
+          "authDetails": [
+            {
+              "registerUType": "SIGNED-JWT",
+              "jwksEndpoint": "https://unauth.cdr.api.anz/cds-au/jwks"
+            }
+          ],
+          "lastUpdated": "2021-03-02T06:05:06Z"
+        },
+        {
+          "dataHolderBrandId": "7c356d3d-ff7d-49a5-b16e-3cffca4e49d3",
+          "brandName": "CommBank",
+          "industry": "banking",
+          "logoUri": "https://www.commbank.com.au/content/dam/commbank-assets/cba-stacked.jpg",
+          "legalEntity": {
+            "legalEntityId": "fc428f71-847c-4975-8dd6-e3f2cfdaafd0",
+            "legalEntityName": "Commonwealth Bank of Australia",
+            "logoUri": "https://www.commbank.com.au/content/dam/commbank-assets/cba-stacked.jpg",
+            "abn": "48123123124"
+          },
+          "status": "ACTIVE",
+          "endpointDetail": {
+            "publicBaseUri": "https://api.commbank.com.au/public",
+            "resourceBaseUri": "https://secure.api.commbank.com.au/api",
+            "infosecBaseUri": "https://api.commbank.com.au/infosec",
+            "websiteUri": "https://www.commbank.com.au"
+          },
+          "authDetails": [
+            {
+              "registerUType": "SIGNED-JWT",
+              "jwksEndpoint": "https://api.commbank.com.au/infosec/.well-known/openid-configuration/jwks"
+            }
+          ],
+          "lastUpdated": "2021-03-02T05:59:45Z"
+        },
+        {
+          "dataHolderBrandId": "bb91c794-e92e-4f9c-ba03-d83dea29fca7",
+          "brandName": "Regional Australia Bank",
+          "industry": "banking",
+          "logoUri": "https://www.regionalaustraliabank.com.au/-/media/CommunityMutual/Images/Logo/regional-australia-bank-primary-logo.png",
+          "legalEntity": {
+            "legalEntityId": "d4c2167c-03dc-4ba2-a03d-55aa34632e5e",
+            "legalEntityName": "Regional Australia Bank Ltd.",
+            "logoUri": "https://www.regionalaustraliabank.com.au/-/media/CommunityMutual/Images/Logo/regional-australia-bank-primary-logo.png",
+            "abn": "21087650360"
+          },
+          "status": "ACTIVE",
+          "endpointDetail": {
+            "publicBaseUri": "https://public-data.cdr.regaustbank.io",
+            "resourceBaseUri": "https://secured-data.cdr.regaustbank.io",
+            "infosecBaseUri": "https://idp.cdr.regionalaustraliabank.com.au/v1/rab-cdr",
+            "extensionBaseUri": "https://secured-data.cdr.regaustbank.io",
+            "websiteUri": "https://www.regionalaustraliabank.com.au/"
+          },
+          "authDetails": [
+            {
+              "registerUType": "SIGNED-JWT",
+              "jwksEndpoint": "https://idp.cdr.regionalaustraliabank.com.au/v1/rab-cdr/jwks"
+            }
+          ],
+          "lastUpdated": "2021-03-02T06:27:23Z"
+        }
+      ]
+      
+
+
     results.push([badBank,inactiveBank])
+    results.push(realDhs)
 
 
     return _.flatten(results);
